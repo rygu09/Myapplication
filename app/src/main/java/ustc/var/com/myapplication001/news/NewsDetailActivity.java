ac0bc0cd2f545aabb52e6fb230c86a404cf92f84
rygu09 <rygu09@foxmail.com>
2017-10-22 11:30:15 +0800

    开始做newsdetail 感觉还是要用MVP 准备改成MVP

--- app/src/main/java/ustc/var/com/myapplication001/news/NewsDetailActivity.java
+++ app/src/main/java/ustc/var/com/myapplication001/news/NewsDetailActivity.java
@@ -1,9 +1,76 @@
 package ustc.var.com.myapplication001.news;
 
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import org.sufficientlysecure.htmltextview.HtmlTextView;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+import ustc.var.com.myapplication001.R;
+import ustc.var.com.myapplication001.bean.NewsBean;
+import ustc.var.com.myapplication001.util.ImageLoaderUtils;
+import ustc.var.com.myapplication001.util.ToolUtils;
+
 /**
  *
  * Created by GRY on 2017/10/22.
  */
 
-public class NewsDetailActivity {
+public class NewsDetailActivity extends SwipeBackActivity {
+    private NewsBean mNews;
+    private HtmlTextView mTVNewsContent;
+    private ProgressBar mProgressBar;
+    private SwipeBackLayout mSwipeBackLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
+        mTVNewsContent = (HtmlTextView) findViewById(R.id.htNewsContent);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        mSwipeBackLayout = getSwipeBackLayout();
+        mSwipeBackLayout.setEdgeSize(ToolUtils.getWidthInPx(this));
+        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
+
+        mNews = (NewsBean) getIntent().getSerializableExtra("news");
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(mNews.getTitle());
+
+        ImageLoaderUtils.display(getApplicationContext(), (ImageView) findViewById(R.id.ivImage), mNews.getImgsrc());
+
+//        mNewsDetailPresenter = new NewsDetailPresenterImpl(getApplication(), this);
+//        mNewsDetailPresenter.loadNewsDetail(mNews.getDocid());
+    }
+
+
+    public void showNewsDetialContent(String newsDetailContent) {
+        mTVNewsContent.setHtmlFromString(newsDetailContent, new HtmlTextView.LocalImageGetter());
+    }
+
+
+    public void showProgress() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+
+    public void hideProgress() {
+        mProgressBar.setVisibility(View.GONE);
+    }
 }
