466c621c618d394dbcb4590a3d1e14e9d340fa7c
rygu09 <rygu09@foxmail.com>
2017-10-24 16:49:16 +0800

    主界面的上滑toolbar隐藏功能完成 1、toolbar app:layout_scrollFlags=”scroll|enterAlways” 属性 2、可滑动的控件里 app:layout_behavior="@string/appbar_scrolling_view_behavior">

--- app/src/main/java/ustc/var/com/myapplication001/MainActivity.java
+++ app/src/main/java/ustc/var/com/myapplication001/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
